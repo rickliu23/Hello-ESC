5e97e72c3381ebcf94a78571b707d899f2ef1ec9
RickLiu <rickliu95@gmail.com>
2026-06-13 10:31:03 +0800

    增加AM32的固件代码

---
+++ reference/code/AM32-MultiRotor-ESC-firmware-2.01/Mcu/l431/Inc/peripherals.h
@@ -0,0 +1,32 @@
+/*
+ * peripherals.h
+ *
+ *  Created on: Sep. 26, 2020
+ *      Author: Alka
+ */
+
+#ifndef PERIPHERALS_H_
+#define PERIPHERALS_H_
+
+
+
+#endif /* PERIPHERALS_H_ */
+
+#include "main.h"
+
+void initAfterJump(void);
+void initCorePeripherals(void);
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+void MX_DMA_Init(void);
+void MX_ADC1_Init(void);
+void MX_COMP2_Init(void);
+void MX_TIM1_Init(void);
+void MX_TIM2_Init(void);
+void MX_TIM15_Init(void);
+void MX_TIM16_Init(void);
+void MX_TIM7_Init(void);
+void MX_TIM6_Init(void);
+void MX_IWDG_Init(void);
+
+
---
+++ reference/code/readme.md
@@ -0,0 +1,3 @@
++ AM32-MultiRotor-ESC-firmware-2.01
+  + AM32的固件代码，V2.01
+  + 原仓库地址：[Releases · AlkaMotors/AM32-MultiRotor-ESC-firmware](https://github.com/AlkaMotors/AM32-MultiRotor-ESC-firmware/releases/)
